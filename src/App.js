3370c2e77f884650d75532cfc5b104813e0f9c6c
Yasniel Fajardo Egues <54322191+yasniel1408@users.noreply.github.com>
2021-05-30 20:42:26 -0400

    login facebook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import FacebookLogin from "react-facebook-login";
+import { Card, Image } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState("");
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: "600px" }}>
+        <Card.Header>
+          {!login && (
+            <FacebookLogin
+              appId="940450353404968"
+              // autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              icon="fa-facebook"
+            />
+          )}
+          {login && <Image src={picture} roundedCircle width={100}/>}
+        </Card.Header>
+        {login && (
+          <Card.Body>
+            <Card.Title style={{fontSize: 50}}>{data.name}</Card.Title>
+            <Card.Text>{data.email}</Card.Text>
+          </Card.Body>
+        )}
+      </Card>
     </div>
   );
 }
